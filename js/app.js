63ec8b671214cb6e11940596e9ac18499b89c706
EngTahaAbdo <56361670+EngTahaAbdo@users.noreply.github.com>
2019-12-16 22:26:06 +0200

    hover

--- js/app.js
+++ js/app.js
@@ -1,6 +1,7 @@
 const title = $('#title');
 const price =  $('#price');
 const description = $('#describtion');
+const input = document.querySelector('#image');
 
 
 $('.create-post').on('click', function(event) {
@@ -9,11 +10,13 @@ $('.create-post').on('click', function(event) {
 	let validTitle = validate(title);
 	let validPrice = validate(price);
 	let validDescription = validate(description);
+	let validImage = validateImage(input);
 
-
+	console.log(validImage)
+	console.log(input)
 	event.preventDefault();
 
-	if (validTitle&&validPrice&&validDescription) {
+	if (validTitle&&validPrice&&validDescription&&validImage) {
 
 			$('form').submit();
 
@@ -34,3 +37,28 @@ function validate(input) {
 			return true ;
 	}
 }
+
+
+
+
+console.log(input)
+console.log(input.files)
+
+function validateImage(input) {
+	if (input.files.length === 0 ){
+		input.classList.add('is-invalid');
+		input.classList.remove('is-valid');
+		return false ;
+	}
+	else {
+			input.classList.remove('is-invalid');
+			input.classList.add('is-valid');
+			return true ;
+	}
+}
+
+
+
+// input.addEventListener('change',function(){
+// 	console.log('')
+// })
